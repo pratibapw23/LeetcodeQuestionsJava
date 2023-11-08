92a99fcd3e94fe422218d2bc555ed7a3cb1d13c3
Pratibha <pratipw23@gmail.com>
2023-11-08 21:09:47 +0530

    Arrays:Added problem statement description

--- src/com/leetcodeQuestions/TwoSum.java
+++ src/com/leetcodeQuestions/TwoSum.java
@@ -1,3 +1,8 @@
+/*
+ Given an array of integers nums and an integer target, 
+ return indices of the two numbers such that they add up to target 
+*/
+
 package com.leetcodeQuestions;
 
 public class TwoSum {
