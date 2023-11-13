0c61089fc23725f5ee050f6d0991623d351fc3bf
Pratibha <pratipw23@gmail.com>
2023-11-13 13:37:25 +0530

    Arrays:Tried different solution for 'BestTimeToBuyAndSellStock' problem statement:BruteForce-212/212 TCs passed

--- src/com/leetcodeQuestions/BestTimeToBuyAndSellStock.java
+++ src/com/leetcodeQuestions/BestTimeToBuyAndSellStock.java
@@ -18,48 +18,63 @@ public class BestTimeToBuyAndSellStock {
 
 	private static int maxProfit(int[] prices) {
 		
-		LinkedHashMap<Integer, Integer> map=new LinkedHashMap<>();
-		ArrayList<Integer> list=new ArrayList<>();
+		int max=Integer.MAX_VALUE;
 		int maxdiff=0;
-		for(int i=0;i<prices.length;i++) {
-			int current=prices[i];
-			boolean selected =false;
-			for(int j=i+1;j<prices.length;j++) {
-				if(prices[j]>current) {
-					selected=true;
-				}
-			}
-			if(selected==true) {
-				for(int j=i+1;j<prices.length;j++)
-				{
-					list.add(prices[j]);
-				}
-			}
-			int max=0;
-			if(!list.isEmpty()) {
-				max=Collections.max(list);
-			}
-			
-			if(selected==true)
-			{
-				map.put(current, max);
-				for(Map.Entry<Integer, Integer> entry:map.entrySet()) {
-					int diff=entry.getValue()-entry.getKey();
-					if(diff>maxdiff)
-						maxdiff=diff;
-				}
-			}
-			list.clear();
-		}
-//		int maxdiff=0;
-//		for(Map.Entry<Integer, Integer> entry:map.entrySet()) {
-//			int diff=entry.getValue()-entry.getKey();
-//			if(diff>maxdiff)
-//				maxdiff=diff;
-//		}
-		System.out.println(map);
+		int diff=0;
 		
-		return maxdiff;
+		for(int i=0;i<prices.length;i++) {
+			if(prices[i]<max) {
+				max=prices[i];
+			}
+			maxdiff=prices[i]-max;
+			System.out.println(prices[i]+"-"+max);
+			if(diff<maxdiff)
+				diff=maxdiff;
+		}
+		
+//		LinkedHashMap<Integer, Integer> map=new LinkedHashMap<>();
+//		ArrayList<Integer> list=new ArrayList<>();
+//		int maxdiff=0;
+//		for(int i=0;i<prices.length;i++) {
+//			int current=prices[i];
+//			boolean selected =false;
+//			for(int j=i+1;j<prices.length;j++) {
+//				if(prices[j]>current) {
+//					selected=true;
+//				}
+//			}
+//			if(selected==true) {
+//				for(int j=i+1;j<prices.length;j++)
+//				{
+//					list.add(prices[j]);
+//				}
+//			}
+//			int max=0;
+//			if(!list.isEmpty()) {
+//				max=Collections.max(list);
+//			}
+//			
+//			if(selected==true)
+//			{
+//				map.put(current, max);
+//				for(Map.Entry<Integer, Integer> entry:map.entrySet()) {
+//					int diff=entry.getValue()-entry.getKey();
+//					if(diff>maxdiff)
+//						maxdiff=diff;
+//				}
+//			}
+//			list.clear();
+//		}
+////		int maxdiff=0;
+////		for(Map.Entry<Integer, Integer> entry:map.entrySet()) {
+////			int diff=entry.getValue()-entry.getKey();
+////			if(diff>maxdiff)
+////				maxdiff=diff;
+////		}
+//		System.out.println(map);
+//		
+//		return maxdiff;
+		return diff;
 	}
 
 }
