9689f99aecc1c5207924e6e3699b53b449eb5f7d
Pratibha <pratipw23@gmail.com>
2023-12-16 18:31:08 +0530

    Sliding-Window:little Optimized java code

--- src/com/leetcodeQuestions/MinimumSizeSubarraySum.java
+++ src/com/leetcodeQuestions/MinimumSizeSubarraySum.java
@@ -3,33 +3,46 @@ package com.leetcodeQuestions;
 public class MinimumSizeSubarraySum {
 
 	public static void main(String[] args) {
-		int arr[]= {1,4,4};
-		int target=4;
+		int arr[]= {2,3,1,2,4,3};
+		int target=7;
 		int len=findMinimumSizeSubarray(arr,target);
 		System.out.println(len);
 	}
 
 	private static int findMinimumSizeSubarray(int[] arr, int target) {
 		int left=0;
-		int minleft=0;
-		int minlen=0;
+		int minlen=Integer.MAX_VALUE;
 		int sum=0;
-		int right=0;
-		for(right=0;right<arr.length;right++) {
-			if(sum>=target) {
+		
+		for(int right=0;right<arr.length;right++) {
+			sum+=arr[right];
+			while(sum>=target) {
 				minlen=Math.min(minlen, right-left+1);
 				sum-=arr[left];
+				left++;
 			}
-			else
-				sum+=arr[right];
-		while(sum>=target) {
-			minlen=Math.min(minlen, right-left+1);
-			sum-=arr[left];
-			left++;
+			
 		}
-		}
-		System.out.println("Minlength="+minlen);
-		System.out.println("Minleft="+minleft);
+		
+		
+//		for(right=0;right<arr.length;right++) {
+//			if(sum>=target) {
+//				minlen=Math.min(minlen, right-left+1);
+//				sum-=arr[left];
+//			}
+//			else
+//				sum+=arr[right];
+//		while(sum>=target) {
+//			minlen=Math.min(minlen, right-left+1);
+//			sum-=arr[left];
+//			left++;
+//		}
+//		}
+		if(minlen==Integer.MAX_VALUE)
+			return 0;
+//		System.out.println("Minlength="+minlen);
+//		System.out.println("Minleft="+minleft);
+		
 		return minlen;
 	}
 
