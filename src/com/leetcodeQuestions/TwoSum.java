074a150377e0ac5964c167c8676c2223441a8c78
Pratibha <pratipw23@gmail.com>
2023-12-25 22:21:59 +0530

    Two-Pointers:Optimized Java Solution

--- src/com/leetcodeQuestions/TwoSum.java
+++ src/com/leetcodeQuestions/TwoSum.java
@@ -5,6 +5,8 @@
 
 package com.leetcodeQuestions;
 
+import java.util.Arrays;
+
 public class TwoSum {
 
 	public static void main(String[] args) {
@@ -17,18 +19,27 @@ public class TwoSum {
 	}
 
 	private static int[] twoSum(int[] nums, int target) {
-		int indices[]=new int[2];
-		for(int i=0;i<nums.length;i++) {
-			int sum=0;
-			for(int j=i+1;j<nums.length;j++) {
-				sum=nums[i]+nums[j];
-				if(sum==target) {
-					indices[0]=i+1;
-					indices[1]=j+1;
-				}
-			}
+//		int indices[]=new int[2];
+		int left=0;
+		Arrays.sort(nums);
+		int right=nums.length-1;
+		while(nums[left]+nums[right]!=target) {
+			if(nums[left]+nums[right]<target)
+				left++;
+			else
+				right--;
 		}
-		return indices;
+//		for(int i=0;i<nums.length;i++) {
+//			int sum=0;
+//			for(int j=i+1;j<nums.length;j++) {
+//				sum=nums[i]+nums[j];
+//				if(sum==target) {
+//					indices[0]=i+1;
+//					indices[1]=j+1;
+//				}
+//			}
+//		}
+		return new int[] {left+1,right+1};
 	}
 
 }
