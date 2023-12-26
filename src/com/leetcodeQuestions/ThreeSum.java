31015e3910f86f6b8d57d2242b167d81eb836315
Pratibha <pratipw23@gmail.com>
2023-12-26 22:42:39 +0530

    Arrays:Modifications to return the unique list of triplets

--- src/com/leetcodeQuestions/ThreeSum.java
+++ src/com/leetcodeQuestions/ThreeSum.java
@@ -1,31 +1,33 @@
 package com.leetcodeQuestions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ThreeSum {
 
 	public static void main(String[] args) {
 		
 		int nums[]= {-1,0,1,2,-1,-4};
-		List<ArrayList<Integer>> list=threesum(nums);
+		List<List<Integer>> list=threesum(nums);
 		System.out.println(list);
 	}
 
-	private static List<ArrayList<Integer>> threesum(int[] nums) {
-		List<ArrayList<Integer>> mainlist=new ArrayList<>();
+	private static List<List<Integer>> threesum(int[] nums) {
+		Arrays.sort(nums);
+		Set<List<Integer>> set=new HashSet<>();
 		for(int i=0;i<nums.length;i++) {
 			for(int j=i+1;j<nums.length;j++) {
 				for(int k=j+1;k<nums.length;k++) {
 					if((nums[i]+nums[j]+nums[k])==0) {
 						ArrayList<Integer> list=new ArrayList<>(List.of(nums[i], nums[j], nums[k]));
-						mainlist.add(new ArrayList<>(list));
+						set.addAll(Arrays.asList(list));
 					}
 				}
 			}
 		}
-		System.out.println(mainlist);
-		return null;
+		return new ArrayList<>(set);
 	} 
 }
