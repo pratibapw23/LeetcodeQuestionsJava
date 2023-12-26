c69f2df08da268f865c18df1d3dd6ef6ccb14445
Pratibha <pratipw23@gmail.com>
2023-12-26 22:45:32 +0530

    Two-Pointers:Optimized code to O(n*n) from O(n*n*n)

--- src/com/leetcodeQuestions/ThreeSum.java
+++ src/com/leetcodeQuestions/ThreeSum.java
@@ -18,16 +18,19 @@ public class ThreeSum {
 	private static List<List<Integer>> threesum(int[] nums) {
 		Arrays.sort(nums);
 		Set<List<Integer>> set=new HashSet<>();
-		for(int i=0;i<nums.length;i++) {
-			for(int j=i+1;j<nums.length;j++) {
-				for(int k=j+1;k<nums.length;k++) {
-					if((nums[i]+nums[j]+nums[k])==0) {
-						ArrayList<Integer> list=new ArrayList<>(List.of(nums[i], nums[j], nums[k]));
-						set.addAll(Arrays.asList(list));
-					}
+		for(int i=0;i<nums.length-2;i++) {
+			int j=i+1;
+			int k=nums.length-1;
+			while(j<k) {
+				int sum=nums[j]+nums[k];
+				if(sum==(-nums[i])) {
+					set.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
 				}
+				else if(sum<-nums[i]) j++;
+				else if(sum>-nums[i]) k--;
 			}
 		}
 		return new ArrayList<>(set);
+
 	} 
 }
