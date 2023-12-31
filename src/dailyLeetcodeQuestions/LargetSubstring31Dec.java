d5acb16f7ffac0f4efaaca0c64b11d2d016fb1f5
Pratibha <pratipw23@gmail.com>
2023-12-31 16:23:33 +0530

    Daily-Leetcode:Optimized Solution-java

--- src/dailyLeetcodeQuestions/LargetSubstring31Dec.java
+++ src/dailyLeetcodeQuestions/LargetSubstring31Dec.java
@@ -1,22 +1,25 @@
 package dailyLeetcodeQuestions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LargetSubstring31Dec {
 
 	public static void main(String[] args) {
-		String s="jdhfa";
+		String s="aydsicwrfybunpqsdsnenvrfirr";
 		int len=largestSubstringBetweenTwoEqualCharacters(s);
 		System.out.println(len);
 	}
 
 	private static int largestSubstringBetweenTwoEqualCharacters(String s) {
-		int count=-1;
-		int maxlen=Integer.MIN_VALUE;
+		int maxlen=-1;
+		Map<Character,Integer> map=new HashMap<>();
 		for(int i=0;i<s.length();i++) {
-			for(int j=i+1;j<s.length();j++) {
-				if(s.charAt(i)==s.charAt(j)) {
-					count=j-i-1;
-				}
-				maxlen=Math.max(maxlen, count);
+			if(map.containsKey(s.charAt(i))) {
+				maxlen=Math.max(maxlen, (i-map.get(s.charAt(i))-1));
+			}
+			else {
+				map.put(s.charAt(i), i);
 			}
 		}
 		return maxlen;
