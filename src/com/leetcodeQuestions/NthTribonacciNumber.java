babc6f76d46df23b71243d102720491d0e4d4a97
Pratibha <pratipw23@gmail.com>
2024-01-08 13:46:22 +0530

    DP:Java solution for 'Nth Tribonacci Number' leetcode question

---
+++ src/com/leetcodeQuestions/NthTribonacciNumber.java
@@ -0,0 +1,38 @@
+package com.leetcodeQuestions;
+
+import java.util.Arrays;
+
+public class NthTribonacciNumber {
+
+	public static void main(String[] args) {
+		
+		int n=25;
+		int num=tribonacciNumber(n);
+		System.out.println(num);
+
+	}
+
+	private static int tribonacciNumber(int n) {
+		if(n<=0)
+			return 0;
+        if(n==1)
+            return 1;
+        int dp[]=new int[n+1];
+		for(int i=0;i<n+1;i++)
+			dp[i]=0;
+		dp[0]=0;
+		dp[1]=1;
+		dp[2]=1;
+		int a=0,b=0,c=0;
+		for(int i=3;i<n+1;i++) {
+			c=i-1; //1
+			b=i-2; //0
+			a=i-3;
+			dp[i]=dp[a]+dp[b]+dp[c];
+			a++;b++;c++;
+		}
+//		System.out.println(Arrays.toString(dp));
+		return dp[dp.length-1];
+	}
+
+}
