a44ab8ac16550a098ed9a6103742410389e4549b
Pratibha <pratipw23@gmail.com>
2024-01-15 22:48:20 +0530

    Strings:Java solution for 'Reverse String' leetcode question

---
+++ src/com/leetcodeQuestions/ReverseString.java
@@ -0,0 +1,26 @@
+package com.leetcodeQuestions;
+
+import java.util.Arrays;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		char s[]= {'h','e','l','l','o'};
+		reverseString(s);
+	}
+
+	private static void reverseString(char[] s) {
+
+		int left=0;
+		int right=s.length-1;
+		while(left<right) {
+			char temp=s[left];
+			s[left]=s[right];
+			s[right]=temp;
+			left++;
+			right--;
+		}
+		System.out.println(Arrays.toString(s));
+	}
+
+}
