b8e0836632b078f4f3dc3bc3bea76656e507eb85
Pratibha <pratipw23@gmail.com>
2024-02-14 12:00:17 +0530

    Dailt-Leetcode:Added Little optimized solution for 'Rearrange array elements by it's sign' daily leetcode question

--- src/dailyLeetcodeQuestions2024/RearrangeArrayElementsBySign.java
+++ src/dailyLeetcodeQuestions2024/RearrangeArrayElementsBySign.java
@@ -7,61 +7,27 @@ import java.util.List;
 public class RearrangeArrayElementsBySign {
 
 	public static void main(String[] args) {
-		int nums[]= {1,-1};
+		int nums[]= {3,1,-2,-5,2,-4};
 		int arr[]=rearrangeArrayElementsBySign(nums);
 		System.out.println(Arrays.toString(arr));
 	}
 
 	private static int[] rearrangeArrayElementsBySign(int[] nums) {
-//		List<Integer> positive=new LinkedList<>();
-//		List<Integer> negative=new LinkedList<>();
-		int arr[]=new int[nums.length];
-		int positive[]=new int[nums.length/2];
-		int negative[]=new int[nums.length/2];
-		
-		int pos=0;
-		int neg=0;
+		int ans[]=new int[nums.length];
+		int p=0;
+		int n=1;
 		for(int i=0;i<nums.length;i++) {
 			if(nums[i]>=0) {
-				positive[pos]=nums[i];
-				pos++;
+				ans[p]=nums[i];
+				p+=2;
 			}
-			else
-			{
-				negative[neg]=nums[i];
-				neg++;
+			else {
+				ans[n]=nums[i];
+				n+=2;
 			}
 		}
-		int p=0;
-		for(int i=0;i<positive.length;i++) {
-			arr[p]=positive[i];
-			p+=2;
-		}
-		int n=1;
-		for(int i=0;i<negative.length;i++) {
-			arr[n]=negative[i];
-			n+=2;
-		}
-//		System.out.println(Arrays.toString(arr));
-		
-//		for(int i=0;i<nums.length;i++) {
-//			if(nums[i]>=0)
-//				positive.add(nums[i]);
-//			else
-//				negative.add(nums[i]);
-//		}
-////		System.out.println(positive+" "+negative);
-//		int pos=0;
-//		for(int i=0;i<positive.size();i++) {
-//			arr[pos]=positive.get(i);
-//			pos+=2;
-//		}
-//		int t=1;
-//		for(int i=0;i<negative.size();i++) {
-//			arr[t]=negative.get(i);
-//			t+=2;
-//		}
-		return arr;
+//		System.out.println(Arrays.toString(ans));
+		return ans;
 	}
 
 }
