803ffea3603cfa2aa4e8ec51f69c48a55c64b4e6
Pratibha <pratipw23@gmail.com>
2024-02-19 21:05:10 +0530

    Arrays:Java solution for 'Contains Duplicates' leetcode question - Optimized memory usage

--- src/dailyTargatedLeetcodeQuestions/ContainsDuplicates.java
+++ src/dailyTargatedLeetcodeQuestions/ContainsDuplicates.java
@@ -1,22 +1,29 @@
 package dailyTargatedLeetcodeQuestions;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicates {
 
 	public static void main(String[] args) {
-		int nums[]= {1,2,3,4};
+		int nums[]= {1,2,3,4,2};
 		boolean res=containsDuplicates(nums);
 		System.out.println(res);
 	}
 
 	private static boolean containsDuplicates(int[] nums) {
-		Set<Integer> set=new HashSet<>();
-		for(int i=0;i<nums.length;i++) {
-			if(!set.add(nums[i]))
+//		Set<Integer> set=new HashSet<>();
+//		for(int i=0;i<nums.length;i++) {
+//			if(!set.add(nums[i]))
+//				return true;
+//			set.add(nums[i]);
+//		}
+		Arrays.sort(nums);
+		int n=nums.length;
+		for(int i=1;i<n;i++) {
+			if(nums[i]==nums[i-1])
 				return true;
-			set.add(nums[i]);
 		}
 		return false;
 	}
