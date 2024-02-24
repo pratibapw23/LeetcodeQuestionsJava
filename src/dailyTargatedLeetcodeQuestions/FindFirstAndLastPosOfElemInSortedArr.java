19e336562272c052dbfaefa1db22356362953757
Pratibha <pratipw23@gmail.com>
2024-02-24 23:21:05 +0530

    Array:Java Optimized solution for 'Find first and last position of target element in sorted array' leetcode question

--- src/dailyTargatedLeetcodeQuestions/FindFirstAndLastPosOfElemInSortedArr.java
+++ src/dailyTargatedLeetcodeQuestions/FindFirstAndLastPosOfElemInSortedArr.java
@@ -6,7 +6,7 @@ public class FindFirstAndLastPosOfElemInSortedArr {
 
 	public static void main(String[] args) {
 		int nums[]= {5,7,7,8,8,8,8,8,10};
-		int target=0;
+		int target=8;
 		int arr[]=searchRanges(nums,target);
 		System.out.println(Arrays.toString(arr));
 
@@ -14,28 +14,50 @@ public class FindFirstAndLastPosOfElemInSortedArr {
 
 	private static int[] searchRanges(int[] nums, int target) {
 		
-		int arr[]=new int[2];
-		arr[0]=-1;
-		arr[1]=-1;
+		int firstindex=binarySearchLeft(nums,target);
+		int lastindex=binarySearchRight(nums,target);
 		
-		int firstindex=-1;
-		int lastindex=-1;
-		
-		for(int i=0;i<nums.length;i++) {
-			if(nums[i]==target) {
-				firstindex=i;
-				break;
-			}
-		}
-		for(int i=0;i<nums.length;i++) {
-			if(nums[i]==target) {
-				lastindex=i;
-			}
-		}
-		arr[0]=firstindex;
-		arr[1]=lastindex;
-		return arr;
+		return new int[] {firstindex,lastindex};
 	}
+
+	private static int binarySearchLeft(int[] nums, int target) {
+		int index=-1;
+		int left=0,right=nums.length-1;
+		while(left<=right) {
+			int mid=left+(right-left)/2;
+			
+			if(nums[mid]==target) {
+				index=mid;
+				right=mid-1;
+			}
+			else if(nums[mid]<target) {
+				left=mid+1;
+			}
+			else
+				right=mid-1;
+		}
+		return index;
+	}
+	private static int binarySearchRight(int[] nums, int target) {
+		int index=-1;
+		int left=0,right=nums.length-1;
+		while(left<=right) {
+			int mid=left+(right-left)/2;
+			
+			if(nums[mid]==target) {
+				index=mid;
+//				right=mid-1;
+				left=mid+1;
+			}
+			else if(nums[mid]<target) {
+				left=mid+1;
+			}
+			else
+				right=mid-1;
+		}
+		return index;
+	}
+	
 
 
 }
