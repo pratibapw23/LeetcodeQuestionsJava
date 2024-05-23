b40cfdb4a6ecac4d39a25c393773d547d7fcf7fc
Pratibha <pratipw23@gmail.com>
2024-05-23 23:34:26 +0530

    Daily-Leetcode: java optimized solution for 'Number of substrings contains all characters' leetcode question:Medium

--- dailyTargated/medium/NumberOfSubstringsContainingAllChars.java
+++ dailyTargated/medium/NumberOfSubstringsContainingAllChars.java
@@ -12,29 +12,46 @@ public class NumberOfSubstringsContainingAllChars {
 	}
 
 	private static int numberOfSubstringsContainingAllCharacters(String s) {
+//		int count=0;
+//		for(int i=0;i<s.length();i++) {
+//			for(int j=i+1;j<=s.length();j++) {
+//				if(checkAllChars(s.substring(i, j))) {
+//					count++;
+//				}
+//			}
+//		}
+//		return count;
+		
+		Map<Character, Integer> map=new HashMap<>();
 		int count=0;
+		int j=0;
 		for(int i=0;i<s.length();i++) {
-			for(int j=i+1;j<=s.length();j++) {
-				if(checkAllChars(s.substring(i, j))) {
-					count++;
+			map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+			
+			while(map.size()==3) {
+				count+=s.length()-i;
+				map.put(s.charAt(j), map.get(s.charAt(j))-1);
+				if(map.get(s.charAt(j))==0) {
+					map.remove(s.charAt(j));
 				}
+				j++;
 			}
 		}
 		return count;
 	}
 
-	public static boolean checkAllChars(String substring) {
-		Map<Character,Integer> map=new HashMap<>();
-		map.put('a', 0);map.put('b', 0);map.put('c', 0);
-		for(int i=0;i<substring.length();i++) {
-			if(map.containsKey(substring.charAt(i))) {
-				map.put(substring.charAt(i), 1);
-			}
-		}
-		for(Map.Entry<Character, Integer> entry:map.entrySet()) {
-			if(entry.getValue()<=0) return false;
-		}
-		return true;
-	}
+//	public static boolean checkAllChars(String substring) {
+//		Map<Character,Integer> map=new HashMap<>();
+//		map.put('a', 0);map.put('b', 0);map.put('c', 0);
+//		for(int i=0;i<substring.length();i++) {
+//			if(map.containsKey(substring.charAt(i))) {
+//				map.put(substring.charAt(i), 1);
+//			}
+//		}
+//		for(Map.Entry<Character, Integer> entry:map.entrySet()) {
+//			if(entry.getValue()<=0) return false;
+//		}
+//		return true;
+//	}
 
 }
